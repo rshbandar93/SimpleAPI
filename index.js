fe410fecd5f79556cdaaf3721b18475bc22ea998
bambang Susilo <bambang_susilo@bambangs-MacBook-Pro.local>
2018-08-14 21:08:29 +0700

    Remove script test directory

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hello world from node.js server');
+ res.send('Hello World from Nodejs Server!');
 });
 
 //listen to port 3000 by default
--- test/test.js
+++ test/test.js
@@ -5,6 +5,6 @@ describe('GET /', function() {
  it('respond with hello world from node.js server', function(done) {
 
  //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello world from node.js server', done);
+ request(app).get('/').expect('Hello World from Nodejs Server!', done);
  });
 });
